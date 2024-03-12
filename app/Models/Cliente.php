833eb1b745ca74734d260e0f237c65bf14b8ee2d
Noé Medina González <medinagonzaleznoe@gmail.com>
2024-03-12 19:02:31 +0100

    He creado el controlador de clientes

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -2,11 +2,44 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Cliente extends Model {
-    public function vehiculos() {
-        return $this->hasMany(Vehiculo::class);
+/**
+ * Class Cliente
+ *
+ * @property $id
+ * @property $nombre
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property Vehiculo[] $vehiculos
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Cliente extends Model
+{
+    
+    static $rules = [
+		'nombre' => 'required|string',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function vehiculos()
+    {
+        return $this->hasMany(\App\Models\Vehiculo::class, 'id', 'cliente_id');
     }
+    
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('clientes', ClienteController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
