749c0a55c3397b8177aaaf4d30e1182c783f4d63
PinochoBionico1 <pinochobionico@gmail.com>
2024-03-12 12:25:10 +0100

    Rellenado del modelo Reparacion

--- app/Models/Reparacion.php
+++ app/Models/Reparacion.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Reparacion extends Model
-{
-    use HasFactory;
+class Reparacion extends Model {
+    public function vehiculo() {
+        return $this->belongsTo(Vehiculo::class);
+    }
 }
