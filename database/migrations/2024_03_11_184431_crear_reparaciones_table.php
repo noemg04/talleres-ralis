a5262e9f4b9f2c890ed4afd5e7f98fa58ef4ff64
PinochoBionico1 <pinochobionico@gmail.com>
2024-03-12 12:06:23 +0100

    Añadido CrearReparaciones a las migrations

--- database/migrations/2024_03_11_184431_crear_reparaciones_table.php
+++ database/migrations/2024_03_11_184431_crear_reparaciones_table.php
@@ -4,21 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CrearReparacionesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        //
+        Schema::create('reparaciones', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vehiculo_id');
+            $table->string('descripcion');
+            $table->timestamps();
+
+            $table->foreign('vehiculo_id')->references('id')->on('vehiculos')->onDelete('cascade');
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
-        //
+        Schema::dropIfExists('reparaciones');
     }
-};
+}
